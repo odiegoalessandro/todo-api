d3a5ce60beab9e38306e9294a919238df367c083
Diego alessandro da cruz martins <diegoalessandrodacruzmartins@gmail.com>
2022-04-12 14:08:53 -0300

    chore: add nestjs/swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,12 +14,12 @@ import { UsersModule } from "./users/users.module"
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
-      type: process.env.TYPEORM_CONNECTION,
-      host: process.env.TYPEORM_HOST,
-      port: process.env.TYPEORM_PORT,
-      username: process.env.TYPEORM_USER,
-      password: process.env.TYPEORM_PASSWORD,
-      database: process.env.TYPEORM_DATABASE,
+      type: "mysql",
+      host: "localhost",
+      port: 3306,
+      username: "root",
+      password: "gadi338624",
+      database: "todo_app",
       entities: [UserEntity, TokenEntity, TodoEntity],
       synchronize: true,
       autoLoadEntities: true,
